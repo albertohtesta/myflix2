f8f1fada618d4708d78fd3463d98e13be3155c68
albertohtesta <albertohtesta@gmail.com>
2016-10-08 17:36:20 +0000

    Pre-course Work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
+
+  root to: 'videos#index'
+
+    get 'ui(/:action)', controller: 'ui'
+
+    get '/home', to: 'videos#index'
+
+    resources :videos, only: [:show]
+    resources :categories, only: [:show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+classic = Category.create(name: 'Classic')
+family = Category.create(name: 'Family')
+
+videos = Video.create([{title: 'Futurama', small_cover_url: 'tmp/futurama.jpg', 
+	large_cover_url: 'tmp/monk_large.jpg', category: classic, 
+	description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another." },
+ {title: 'South Park', small_cover_url: 'tmp/futurama.jpg',
+  large_cover_url: 'tmp/monk_large.jpg.jpg', category: family,
+   description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999." },
+ {title: 'Family Guy', small_cover_url: 'tmp/family_guy.jpg',
+  large_cover_url: 'tmp/monk_large.jpg.jpg', category: family,
+   description:"Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way."  }])
