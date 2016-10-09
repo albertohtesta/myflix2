7caaf6dcbbec4b37b1e7748c4a0abbb3ef19203e
albertohtesta <albertohtesta@gmail.com>
2016-10-09 13:27:12 +0000

    video model spec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,11 +2,40 @@ require 'spec_helper'
 
 describe Video do
 	
-   it "saves itself" do 
-   	video = Video.new(title: "monk", description: "a great video !")
-   	video.save
+it {should belong_to(:category)}
+it {should validate_presence_of(:title)}
+it {should validate_presence_of(:description)}
 
-   	expect(Video.first).to eq(video)
-   end
+	describe "search_by_title" do
+		it "return an empty array if there is no match" do
+			futurama = Video.create(title: "Futurama", description: "Space travel!")
+			back_to_future = Video.create(title: "back to future", description: "Time travel!")
+			expect(Video.search_by_title("hello")).to eq([])
+		end
+		it "returns an array of one video for an exact match" do
+			futurama = Video.create(title: "Futurama", description: "Space travel!")
+			back_to_future = Video.create(title: "back to future", description: "Time travel!")
+			expect(Video.search_by_title("Futurama")).to eq([futurama])
+			
+		end
+		it "returns an array of one video for a partial match" do
+			futurama = Video.create(title: "Futurama", description: "Space travel!")
+			back_to_future = Video.create(title: "back to Future", description: "Time travel!")
+			expect(Video.search_by_title("urama")).to eq([futurama])
+	
+		end
+		it "returns an array of all matches order by created_at" do
+			futurama = Video.create(title: "Futurama", description: "Space travel!", created_at: 1.day.ago)
+			back_to_future = Video.create(title: "back to Future", description: "Time travel!")
+			expect(Video.search_by_title("Futur")).to eq([back_to_future, futurama])
+
+		end
+		it "returns an empty array for a search with an empty string" do
+			futurama = Video.create(title: "Futurama", description: "Space travel!", created_at: 1.day.ago)
+			back_to_future = Video.create(title: "back to Future", description: "Time travel!")
+			expect(Video.search_by_title("")).to eq([])
+
+		end
+		end
 
 end
